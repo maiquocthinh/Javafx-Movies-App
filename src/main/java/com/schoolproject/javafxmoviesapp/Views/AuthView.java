30117f7960405b5e9cde551c83772da1db62412e
phamvangjang <giangphamvan48@gmail.com>
2023-04-14 09:29:36 +0700

    update authview

--- src/main/java/com/schoolproject/javafxmoviesapp/Views/AuthView.java
+++ src/main/java/com/schoolproject/javafxmoviesapp/Views/AuthView.java
@@ -1,5 +1,6 @@
 package com.schoolproject.javafxmoviesapp.Views;
 
+import com.schoolproject.javafxmoviesapp.Controllers.Auth.ChangePasswordController;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -12,27 +13,6 @@ public class AuthView {
         if(instance == null) instance = new AuthView();
         return instance;
     }
-    public void switchToForgotPassword(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/ForgetPassword.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.setTitle("Forgot Password");
-        if(!stage.isShowing()) stage.show();
-    }
-    public  void handleSubmit(Stage stage) throws  IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/OTP.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.setTitle("Input OTP");
-        if (!stage.isShowing()) stage.show();
-    }
-    public void handleSubmitOTP(Stage stage) throws  IOException{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/ChangePassword.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setScene(scene);
-        stage.setTitle("Reset Password");
-        if(!stage.isShowing()) stage.show();
-    }
     public void switchToLogin(Stage stage ) throws  IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -40,6 +20,7 @@ public class AuthView {
         stage.setTitle("Login");
         if (!stage.isShowing()) stage.show();
     }
+
     public void switchToSignup(Stage stage) throws  IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/Register.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -47,6 +28,26 @@ public class AuthView {
         stage.setTitle("Register");
         if(!stage.isShowing()) stage.show();
     }
+
+    public void switchToForgotPassword(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/ForgetPassword.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Forgot Password");
+        if(!stage.isShowing()) stage.show();
+    }
+
+    public void switchToChangePassword(Stage stage, String email) throws  IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/ChangePassword.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("Change Password");
+        ChangePasswordController changePasswordController = fxmlLoader.getController();
+        changePasswordController.setEmail(email);
+        if(!stage.isShowing()) stage.show();
+    }
+
+
     public void backToLogin(Stage stage) throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -54,6 +55,7 @@ public class AuthView {
         stage.setTitle("Login");
         if(!stage.isShowing()) stage.show();
     }
+
     public void backToForgotPassword(Stage stage)throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/ForgetPassword.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
@@ -61,6 +63,7 @@ public class AuthView {
         stage.setTitle("Forgot Password");
         if(!stage.isShowing()) stage.show();
     }
+
     public void backToInputOTP(Stage stage)throws IOException{
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Auth/OTP.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
