84e6b91abe778a9da1d49dfd5c67b51fa329e76f
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-04-17 22:05:29 +0700

    ui watching and moviedetail

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -29,6 +29,7 @@ module com.schoolproject.javafxmoviesapp {
     opens com.schoolproject.javafxmoviesapp.Controllers to javafx.fxml;
     opens com.schoolproject.javafxmoviesapp.Controllers.Admin to javafx.fxml;
     opens com.schoolproject.javafxmoviesapp.Controllers.Client to javafx.fxml;
+
     exports com.schoolproject.javafxmoviesapp;
     exports com.schoolproject.javafxmoviesapp.Entity;
 }
